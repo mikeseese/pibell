6f90bf6e91ed35fdd02d2aa7df20777ec9dc8f65
Mike Seese <seesemichaelj@gmail.com>
2018-07-22 18:18:20 -0400

    pull correct stretch key

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Add PPA keys
+curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add - &&\
+  sed -i '1s;^;deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main\n;' /etc/apt/sources.list
+
 # Install dependencies
 apt-get update -q &&\
   apt-get install -yq \
